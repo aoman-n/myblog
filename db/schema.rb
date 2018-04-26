0ddb2026dd1e56334e83cbe26d54a33fff500c5b
first commit <>
2018-04-26 20:09:20 +0900

    Goods_table create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426090312) do
+ActiveRecord::Schema.define(version: 20180426110712) do
 
   create_table "article_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "article_id"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20180426090312) do
     t.datetime "updated_at",               null: false
     t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
+
+  create_table "goods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_goods_on_article_id", using: :btree
+    t.index ["user_id"], name: "index_goods_on_user_id", using: :btree
   end
 
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -72,4 +81,6 @@ ActiveRecord::Schema.define(version: 20180426090312) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "goods", "articles"
+  add_foreign_key "goods", "users"
 end
