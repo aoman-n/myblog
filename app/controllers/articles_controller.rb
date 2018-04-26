535103a7be28fcff19bd2a523848a89505663377
first commit <>
2018-04-26 15:08:42 +0900

    article_create view function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :image)
+  end
+
 end
