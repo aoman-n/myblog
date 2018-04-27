da58d9394bc86d7306b2a8080c51275e7a283895
first commit <>
2018-04-27 22:42:30 +0900

    article destroy method function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,16 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    if (current_user.id == article.user_id)
+      article.destroy
+    end
+  end
+
+  def update
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :body, :image).merge( user_id: current_user.id)
