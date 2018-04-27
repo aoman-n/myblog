b6cced9abecea02825fb262721f42b0b938c2a87
first commit <>
2018-04-28 01:16:23 +0900

    comment function add

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to controller: :articles, action: :index
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, article_id: params[:article_id])
+  end
+
 end
