036a4a08e49c0c94e9a88369b5d338ee93e5341a
first commit <>
2018-04-27 20:14:20 +0900

    associations add

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :comments
+  has_many :tags, through: :article_tags
+  has_many :article_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
   has_many :articles
+  has_many :comments
 end
