d78835f5f566205a6765b12ddd7daeb58105e6fd
first commit <>
2018-04-28 21:28:59 +0900

    ranking function add

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,10 @@
 class RankingController < ApplicationController
   before_action :ranking
   def ranking
-    article_ids = Good.group(:article_id).order('count_article_id').limit(5).count(:article_id).keys
-    @ranking = article_ids.map{|id| Article.find(id)}
+    article_ids = Good.group(:article_id).order('count_article_id DESC').limit(5).count(:article_id).keys
+    @good_ranking = article_ids.map{|id| Article.find(id)}
+
+    tag_ids = ArticleTag.group(:tag_id).order('count_tag_id DESC').limit(5).count(:tag_id).keys
+    @tag_ranking = tag_ids.map{|id| Tag.find(id)}
   end
 end
