64a6280885544a3064bee99239f11b485f2c3712
first commit <>
2018-04-30 16:31:09 +0900

    search function add

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < RankingController
     @articles = Article.order('id DESC').limit(10)
   end
 
+  def search
+    @articles = Article.where('title LIKE(?)', "%#{params[:q]}%")
+  end
+
   def show
     @article = Article.find(params[:id])
     @comments = @article.comments.includes(:user)
